ea71cd5b2b975222f456ac022ff05d73df63d168
kwliang <kevwuliang@gmail.com>
2021-09-24 00:24:27 -0400

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const Web3 = require("web3");
 const moment = require("moment");
 const web3 = new Web3('https://eth-mainnet.alchemyapi.io/v2/fH3RqImWKcY75pC03EOuzKF44cbZdJca');
 
-
+// const sdk = require('api')('@opensea/v1.0#gbq4cz1cksxopxqw');
+const axios = require('axios');
 
 const getAllHolders = async ({address:contractAddress = ''} = {}) => {
 	const loggingTag = `[getAllHolders]`;
@@ -16,6 +17,8 @@ const getAllHolders = async ({address:contractAddress = ''} = {}) => {
 			fromBlock: 0,
 			toBlock: 'latest'
 		});
+		
+		//building a map of each token in the collection, and the transfers of ownership 9.21.21 KL
 		for(let i = 0; i < events.length; i++){
 		// for(let i = 0; i < 10; i++){
 			const eventInfo = events[i];
@@ -23,11 +26,6 @@ const getAllHolders = async ({address:contractAddress = ''} = {}) => {
 			// console.info(`${loggingTag} return values`, eventInfo.returnValues);
 			const details = eventInfo.returnValues,
 				tokenID = details.tokenId;
-			
-			if(tokenID == 398){
-				console.info(`${loggingTag} event info`, eventInfo);
-			}
-			// console.info(`${loggingTag} tokenID: ${tokenID}`);
 
 			if(!(tokenID in tokenOwnershipMapObj)){
 				// console.info(`${loggingTag} new event type found! pushing type ("${tokenID}") to array!`);
@@ -54,7 +52,7 @@ const getAllHolders = async ({address:contractAddress = ''} = {}) => {
 			
 			try{
 				transaction = await web3.eth.getTransaction(latestTransfer.transactionHash);
-				console.info(`${loggingTag} latest transaction of token w/ ID ${id}`, transaction);
+				// console.info(`${loggingTag} latest transaction of token w/ ID ${id}`, transaction);
 			} catch(e){
 				console.error(`${loggingTag} Error getting transaction`, e);
 			}
@@ -62,41 +60,152 @@ const getAllHolders = async ({address:contractAddress = ''} = {}) => {
 			return transaction;
 		}
 		
-		const getTimestampOfTransaction = async (id) => {
+		const getTimestampOfLatestTransaction = async (id) => {
 			let timestamp = 0
 			try{
 				const latestTransaction = await getTransactionOfLastTransfer(id);
 				const latestTransactionBlock = await web3.eth.getBlock(latestTransaction.blockHash);
 				timestamp = moment.unix(latestTransactionBlock.timestamp);
-				console.info(`Timestamp of last transaction: ${timestamp}`);
+				// console.info(`Timestamp of last transaction: ${timestamp}`);
 			} catch(e){
 				console.error(`${loggingTag} Error getting timestamp of transaction for token w/ ID ${id}`, e);
 			}
 			return timestamp;
 		}
 		
-		const tokenIDToRetrieveOwnerOf = 2634;
+		const getRandomInt = (max) => {
+			return Math.floor(Math.random() * max);
+		}
 		
-		console.info(`${loggingTag} current owner of tokenID: ${tokenIDToRetrieveOwnerOf}`, addressOfCurrentOwner(tokenIDToRetrieveOwnerOf));
-		Object.keys(tokenOwnershipMapObj).forEach(key => {
-			if(tokenOwnershipMapObj[key].length > 1){
-				console.info(`${loggingTag} token w/ ID: ${key} has more than 1 transaction!`, tokenOwnershipMapObj[key]);
+		const generateListOfIDs = ({num=0}) => {
+			let array = [];
+			for(let i=0; i < num; i++){
+				array.push(getRandomInt(Object.keys(tokenOwnershipMapObj).length));
 			}
-		});
+			return array;
+		}
 		
+		//for debugging purposes
+		
+		//generating list of random IDs to spot check the owner for
+		// const idsToSpotCheck = generateListOfIDs({num: 5});
+
+		// for(let j =0; j < idsToSpotCheck.length; j++){
+		// 	const tokenIDToRetrieveOwnerOf = idsToSpotCheck[j];
+		// 	console.info(`${loggingTag} current owner of tokenID: ${tokenIDToRetrieveOwnerOf}`, addressOfCurrentOwner(tokenIDToRetrieveOwnerOf));
+		// 	console.info(`${loggingTag} timestamp of latest transaction of tokenID: ${tokenIDToRetrieveOwnerOf}`, await getTimestampOfLatestTransaction(tokenIDToRetrieveOwnerOf));
+		// }
+		
+		//for debugging purposes
+		
+		let tokenOwnerWalletMap = {};
+		const tokenIDs = Object.keys(tokenOwnershipMapObj);
+		//mapping all tokens to each wallet that owns it, and displaying the ts_oldest and ts_newest of each token owned per wallet
+		for(let k = 0; k < tokenIDs.length; k++){
+			const id = tokenIDs[k];
+			const walletAddressOfOwner = addressOfCurrentOwner(id);
+			const tsLastTransaction = await getTimestampOfLatestTransaction(id);
+			console.info(`${loggingTag} current owner of tokenID: ${id}`, walletAddressOfOwner);
+			if(!(walletAddressOfOwner in tokenOwnerWalletMap)){
+				tokenOwnerWalletMap[walletAddressOfOwner] = {
+					address: walletAddressOfOwner,
+					ts_oldest_token: tsLastTransaction,
+					ts_latest_token: tsLastTransaction,
+					tokens_owned: [id],
+					num_tokens: 1
+				}
+			} else {//this wallet address has multiple tokens!
+				let existingWalletMapping = tokenOwnerWalletMap[walletAddressOfOwner];
+				console.info(`Already found wallet address: [${walletAddressOfOwner}] in map.`)
+				existingWalletMapping.num_tokens++;
+				existingWalletMapping.tokens_owned.push(id);
+				if(tsLastTransaction < existingWalletMapping.ts_oldest_token){
+					existingWalletMapping.ts_oldest_token = tsLastTransaction;
+				} else if (tsLastTransaction > existingWalletMapping.ts_latest_token){
+					existingWalletMapping.ts_latest_token = tsLastTransaction;
+				}
+				// console.info(`Updated wallet details:`, tokenOwnerWalletMap[walletAddressOfOwner]);
+			}
+			
+			// console.info(`${loggingTag} timestamp of latest transaction of id: ${key}`, await getTimestampOfLatestTransaction(key));
+		}
+		
+		const ownersWalletIDs = Object.keys(tokenOwnerWalletMap);
+		
+		for(let l = 0; l < ownersWalletIDs.length; l++){
+			const address = ownersWalletIDs[l];
+			console.info(`owner wallet details:`, tokenOwnerWalletMap[address]);
+		}
+		// const testWallet = "0xb4742fd474cac814f5297502746c9a97c08a0a2c";
+		// console.info(`Wallet ownership details for address: "${testWallet}"`, tokenOwnerWalletMap[testWallet]);
 		
 	} catch(e){
 		console.error(`${loggingTag} Error:`, e);
 	}
 }
 
-getAllHolders({address: '0x4742D7e66fb868A4fdBedfeA829E611335c0d562'});
+// getAllHolders({address: '0x4742D7e66fb868A4fdBedfeA829E611335c0d562'});
+//contract address of new token 9.20.21 KL
+getAllHolders({address: '0x664ac093f047a131403e9b1cbe3ff1df7e756429'});
 
 const getBalanceOfAddress = async ({address = ''} = {}) => {
+	let balance = 0;
 	const balanceInWei = await web3.eth.getBalance(address);
 	const balanceInEth = web3.utils.fromWei(balanceInWei, 'ether');
-	console.info(balanceInEth);
+	balance = balanceInEth;
+	// console.info(balanceInEth);
+	return balance
 }
 
 //getting balance (in ETH) of a wallet
 // getBalanceOfAddress({address:'0x35f80420bbDB358b6bf274038aeD03B49235E9fC'});
+
+const retrieveEventsByCollectionFromOS = ({contract_address:contractAddress = '', collection_slug:collectionSlug = ''} = {}) => {
+	
+	const options = {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json'
+		},
+		params:{
+			collection_slug:collectionSlug,
+			event_type: 'successful',
+			only_opensea: false,
+			offset: 0,
+			limit: '100'
+		}
+	};
+	
+	axios.get(`https://api.opensea.io/api/v1/events`, options)
+		.then(res => {
+			// console.log(res);
+			if("asset_events" in res.data){
+				const events = res.data.asset_events;
+				if(events.length > 0){
+					// console.info(`success! found ${events.length} events`, events[0]);
+					console.info(`success! found ${events.length} events`);
+					
+				} else {
+					console.error(`Error no asset events found for collection w/ id: ${collectionSlug}`);
+				}
+			}
+		})
+		.catch(err => {
+			console.error(`received error from api:`, err);
+		});
+	// sdk['retrieving-asset-events']({
+	// 	// collection_slug: collectionID,
+	// 	only_opensea: 'false',
+	// 	offset: 0,
+	// 	limit: '20'
+	// })
+	// 	.then(res => {
+	// 		console.log(res);
+	// 	})
+	// 	.catch(err => {
+	// 		console.error(`received error from api:`,err);
+	// 	});
+}
+
+
+// retrieveEventsByCollectionFromOS({collection_slug: 'theimmortalz'});
